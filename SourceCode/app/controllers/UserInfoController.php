de788db06d30204d33dd715176b865024a692ef4
bayusantoso.mail@gmail.com <bayusantoso.mail@gmail.com@fc9d7ee2-55af-60e6-143d-6cd176f430cf>
2014-12-26 11:50:53 +0000

    [BAYU SANTOSO] - COMMIT

--- SourceCode/app/controllers/UserInfoController.php
+++ SourceCode/app/controllers/UserInfoController.php
@@ -1,49 +1,132 @@
 <?php
 
 class UserInfoController extends BaseController {
-
-	/*
-	|--------------------------------------------------------------------------
-	| Default User Info Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'UserInfoController@Index');
-	|
-	*/
-    private $UserInfoService = null;
+    
+    private $UserInfoService;
     
     public function __construct() {
-       parent::__construct();
-       $this->loadDefaultService();
+        parent::__construct();
+        $this->loadDefaultValue();
+        $this->loadDefaultService();
     }
-   
-	public function index()
-	{
+    
+    public function index() {
         $UserInfoList = $this->UserInfoService->getList();
         $this->data['UserInfoList'] = $UserInfoList;
-        return View::make("userinfo\index", $this->data);
-	}
+        return View::make("userinfo/index", $this->data);
+    }
     
-    public function create()
-	{
-        $param = Input::all();
-        $model = $this->bindData($param);
-        return $this->createInputView($model);
-	}
+    public function create() {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowCreate()) { return Redirect::to("access_denied"); }
+        
+        try {
+            $input = Input::all();
+            if (count($input) > 0) {
+                $model = $this->bindData($input);
+                $validation = Validator::make($input, $this->initValidation());
+                if ($validation->fails()) {
+                    return $this->createInputView($model, $validation->messages());
+                } else {
+                    $result = $this->UserInfoService->InsertUserInfo($model);
+                    if (!$result) {
+                        $this->addErrors($this->UserInfoService->getErrors());
+                        return $this->createInputView($model, $validation->messages());
+                    }
+                    return Redirect::to("userinfo/detail/".$model->getFunctionId());
+                }
+            }
+            return $this->createInputView($model);
+        } catch (Exception $ex) {
+            return $this->createInputView(null);
+        }
+    }
     
-    private function createInputView($model) {
-        $this->data['model'] = $model;
-        return View::make("userinfo\input", $this->data);
+    public function edit($id) {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowUpdate()) { return Redirect::to("access_denied"); }
+        
+        $model = $this->UserInfoService->getUserInfo($id);
+        try {
+            $input = Input::all();
+            if (count($input) > 0) {
+                $model = $this->bindData($input);
+                $validation = Validator::make($input, $this->initValidation());
+                if ($validation->fails()) {
+                    return $this->createInputView($model, $validation->messages(), "edit");
+                } else {
+                    $result = $this->UserInfoService->UpdateUserInfo($model, $model->getFunctionId());
+                    if (!$result) {
+                        $this->addErrors($this->UserInfoService->getErrors());
+                        return $this->createInputView($model, $validation->messages(), "edit");
+                    }
+                    return Redirect::to("userinfo/detail/".$model->getFunctionId());
+                }
+            }
+            return $this->createInputView($model, null, "edit");
+        } catch (Exception $ex) {
+            return $this->createInputView(null, null, "edit");
+        }
+    }
+    
+    public function delete($id) {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowDelete()) { return Redirect::to("access_denied"); }
+        try {
+            $model = $this->UserInfoService->getUserInfo($id);
+            if (is_null($model)) { return Redirect::to("userinfo"); }
+            $this->UserInfoService->DeleteUserInfo($id);
+            return Redirect::to("userinfo");
+        } catch (Exception $ex) {
+            var_dump($ex->messages()); die();
+        }
+    }
+    
+    public function detail($id) {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowRead()) { return Redirect::to("access_denied"); }
+        
+        $this->data["model"] = $this->UserInfoService->getUserInfo($id);
+        return View::make("userinfo/detail", $this->data);
+    }
+    
+    private function createInputView($model, $validation = null, $mode = "create") {
+        if (!is_null($model)) {
+            $this->data["model"] = $model;
+        } else {
+            $this->data["model"] = new UserInfo();
+        }
+        if ($mode == "create") {
+            $this->data["action"] = "/userinfo/".$mode;
+        } else {
+            $this->data["action"] = "/userinfo/".$mode."/".(!is_null($model) ? $model->getFunctionId() : "");
+        }
+        
+        $this->addErrorValidation($validation);
+        return View::make("userinfo/input", $this->data);
+    }
+    
+    private function initValidation() {
+        $form_validation = array(
+            "function_id" => "required",
+            "url" => "required"
+        );
+        return $form_validation;
     }
     
     private function bindData($param) {
-        $UserInfo = new UserInfo();
-        
-        return $UserInfo;
+        $UserInfoObj = new UserInfo();
+        if (!is_null($param) && count($param) > 0) {
+            $UserInfoObj->setUserName($param["function_id"]);
+            $UserInfoObj->setUrl($param["url"]);
+            $UserInfoObj->setIsActive($param["is_active"]);
+            $UserInfoObj->setIsShow($param["is_show"]);
+        }
+        return $UserInfoObj;
+    }
+    
+    private function loadDefaultValue() {
+        $this->data["_MODULE_NAME"] = "User Info - ";
     }
     
     private function loadDefaultService() {
--- SourceCode/app/views/functioninfo/detail.blade.php
+++ SourceCode/app/views/functioninfo/detail.blade.php
@@ -14,44 +14,47 @@
     </div>
     <div class="box-content" id="dialog-detailFunctionInfo" title="Detail Function Info">
         <div class="control-group">
-                <label class="control-label">Function Id</label>
-                <div class="controls">
-                    <?php if(!is_null($model)) { echo $model->getFunctionId(); } ?>
-                </div>
+            <label class="control-label">Function Id</label>
+            <div class="controls">
+                <?php if(!is_null($model)) { echo $model->getFunctionId(); } ?>
             </div>
-            <div class="control-group">
-                <label class="control-label">URL</label>
-                <div class="controls">
-                    <?php if(!is_null($model)) { echo $model->getUrl(); } ?>
-                </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label">URL</label>
+            <div class="controls">
+                <?php if(!is_null($model)) { echo $model->getUrl(); } ?>
             </div>
-            <div class="control-group">
-                <label class="control-label">Is Active</label>
-                <?php 
-                    $is_active = "";
-                    if(!is_null($model)) {
-                        if($model->IsActive()) { $is_active = "Yes"; }
-                        else { $is_active = "No"; }
-                    }
-                ?>
-                <div class="controls">
-                    <?php echo $is_active; ?>
-                </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label">Is Active</label>
+            <?php 
+                $is_active = "";
+                if(!is_null($model)) {
+                    if($model->IsActive()) { $is_active = "Yes"; }
+                    else { $is_active = "No"; }
+                }
+            ?>
+            <div class="controls">
+                <?php echo $is_active; ?>
             </div>
-            <div class="control-group">
-                <label class="control-label">Is Show</label>
-                <div class="controls">
-                <?php 
-                    $is_show = "";
-                    if(!is_null($model)) {
-                        if($model->IsShow()) { $is_show = "Yes"; }
-                        else { $is_show = "No"; }
-                    }
-                ?>
-                <?php echo $is_show; ?>
-                </div>
-                        
-            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label">Is Show</label>
+            <div class="controls">
+            <?php 
+                $is_show = "";
+                if(!is_null($model)) {
+                    if($model->IsShow()) { $is_show = "Yes"; }
+                    else { $is_show = "No"; }
+                }
+            ?>
+            <?php echo $is_show; ?>
+            </div>   
+        </div>
+        <div class="form-actions">
+            <button type="button" onclick='window.location.href="<?php echo url()."/functioninfo/edit/".(!is_null($model) ? $model->getFunctionId() : ""); ?>"' class="btn btn-primary">Edit</button>
+            <button type="button" onclick='window.location.href="<?php echo url()."/functioninfo"; ?>"' class="btn btn-primary">Close</button>
+         </div>
     </div>
 </div>
 @stop
