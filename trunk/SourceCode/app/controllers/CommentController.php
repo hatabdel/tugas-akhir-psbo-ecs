ef4c26406f23b1caf666c9cde996cf831649e4ee
ria20farfi@gmail.com <ria20farfi@gmail.com@fc9d7ee2-55af-60e6-143d-6cd176f430cf>
2014-12-29 09:33:18 +0000

    comment

--- trunk/SourceCode/app/controllers/CommentController.php
+++ trunk/SourceCode/app/controllers/CommentController.php
@@ -1,53 +1,133 @@
 <?php
 
 class CommentController extends BaseController {
-
-	/*
-	|--------------------------------------------------------------------------
-	| Default Comment Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'UserInfoController@Index');
-	|
-	*/
-    private $CommentService = null;
+    
+    private $CommentService;
     
     public function __construct() {
-       parent::__construct();
-       $this->loadDefaultService();
+        parent::__construct();
+        $this->loadDefaultValue();
+        $this->loadDefaultService();
     }
-   
-	public function index()
-	{
+    
+    public function index() {
         $CommentList = $this->CommentService->getList();
         $this->data['CommentList'] = $CommentList;
-        return View::make("comment\index", $this->data);
-	}
+        return View::make("comment/index", $this->data);
+    }
     
-    public function create()
-	{
-        $param = Input::all();
-        $model = $this->bindData($param);
-        return $this->createInputView($model);
-	}
+    public function create() {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowCreate()) { return Redirect::to("access_denied"); }
+        
+        try {
+            $input = Input::all();
+            if (count($input) > 0) {
+                $model = $this->bindData($input);
+                $validation = Validator::make($input, $this->initValidation());
+                if ($validation->fails()) {
+                    return $this->createInputView($model, $validation->messages());
+                } else {
+                    $result = $this->CommentService->InsertComment($model);
+                    if (!$result) {
+                        $this->addErrors($this->CommentService->getErrors());
+                        return $this->createInputView($model, $validation->messages());
+                    }
+                    return Redirect::to("comment/detail/".$model->getId());
+                }
+            }
+            return $this->createInputView($model);
+        } catch (Exception $ex) {
+            return $this->createInputView(null);
+        }
+    }
     
-    private function createInputView($model) {
-        $this->data['model'] = $model;
-        return View::make("comment\input", $this->data);
+    public function edit($id) {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowUpdate()) { return Redirect::to("access_denied"); }
+        
+        $model = $this->CommentService->getComment($id);
+        try {
+            $input = Input::all();
+            if (count($input) > 0) {
+                $model = $this->bindData($input);
+                $validation = Validator::make($input, $this->initValidation());
+                if ($validation->fails()) {
+                    return $this->createInputView($model, $validation->messages(), "edit");
+                } else {
+                    $result = $this->CommentService->UpdateComment($model, $model->getId());
+                    if (!$result) {
+                        $this->addErrors($this->CommentService->getErrors());
+                        return $this->createInputView($model, $validation->messages(), "edit");
+                    }
+                    return Redirect::to("Comment/detail/".$model->getId());
+                }
+            }
+            return $this->createInputView($model, null, "edit");
+        } catch (Exception $ex) {
+            return $this->createInputView(null, null, "edit");
+        }
+    }
+    
+    public function delete($id) {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowDelete()) { return Redirect::to("access_denied"); }
+        try {
+            $model = $this->CommentService->getComment($id);
+            if (is_null($model)) { return Redirect::to("Comment"); }
+            $this->CommentService->DeleteComment($id);
+            return Redirect::to("Comment");
+        } catch (Exception $ex) {
+            var_dump($ex->messages()); die();
+        }
+    }
+    
+    public function detail($id) {
+        if (!$this->IsLogin()) { return Redirect::to("login"); }
+        if (!$this->IsAllowRead()) { return Redirect::to("access_denied"); }
+        
+        $this->data["model"] = $this->CommentService->getComment($id);
+        return View::make("comment/detail", $this->data);
+    }
+    
+    private function createInputView($model, $validation = null, $mode = "create") {
+        if (!is_null($model)) {
+            $this->data["model"] = $model;
+        } else {
+            $this->data["model"] = new Comment();
+        }
+        if ($mode == "create") {
+            $this->data["action"] = "/Comment/".$mode;
+        } else {
+            $this->data["action"] = "/Comment/".$mode."/".(!is_null($model) ? $model->getId() : "");
+        }
+        
+        $this->addErrorValidation($validation);
+        return View::make("comment/input", $this->data);
+    }
+    
+    private function initValidation() {
+        $form_validation = array(
+            "content" => "required"
+        );
+        return $form_validation;
     }
     
     private function bindData($param) {
-        $Comment = new Comment();
-        
-        return $Comment;
+        $CommentObj = new Comment();
+        if (!is_null($param) && count($param) > 0) {
+            $CommentObj->setId($param["id"]);
+			$CommentObj->setContent($param["content"]);
+			
+        }
+        return $CommentObj;
+    }
+    
+    private function loadDefaultValue() {
+        $this->data["_MODULE_NAME"] = "Comment - ";
     }
     
     private function loadDefaultService() {
         $this->CommentService = new CommentService();
     }
 }
-?>
