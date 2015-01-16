de5a0a407d894272a3ce029ee70d8b8d2572f082
ria20farfi@gmail.com <ria20farfi@gmail.com@fc9d7ee2-55af-60e6-143d-6cd176f430cf>
2015-01-16 07:07:39 +0000

    undifine function_id di CommentController

--- trunk/SourceCode/app/views/forum/detail.blade.php
+++ trunk/SourceCode/app/views/forum/detail.blade.php
@@ -39,37 +39,7 @@
             </div>
         </div>
 	</tr>
-	<!--</table>
-		<?php
-			$CommentList = array();
-			//if(!is_null($model)) { $CommentList = $model->getComments();}
-		?>
-		
-		@if (count($CommentList) > 0 && !is_null($CommentList))
-		<table class="table table-advance" id="table_comment">
-		<thead>
-			<tr>
-				<th>Title</th>
-				<th>Comment</th>
-				<th>Created Date</th>
-				<th>Created User</th>
-			</tr>
-		</thead>
-		<tbody>
-		@foreach ($CommentList as $item)
-			@if(is_null($item)) continue @endif
-			<tr>
-				<td class="Title"><?php //if (!is_null($item->getTitle())) {echo $item->getTitle();}?> </td>
-				<td class="Content"><?php //if (!is_null($item->getContent())) {echo $item->getContent();}?> </td>
-				<td class="CreatedDate"><?php //if (!is_null($item->getCreatedDate())) { echo $item->getCreatedDate();}?> </td>
-				<td class="CreatedUser"><?php // if (!is_null($item->getCreatedUser())) { echo $item->getCreatedUser();}?> </td>
-			</tr>
-			@endforeach
-		</tbody>
-		</table>
-		@else
-		<!--<h3>There is no comment data in database</h3>
-		@endif-->
+	
         <div class="form-actions">
             <button type="button" onclick='window.location.href="<?php echo url()."/comment/create/".(!is_null($model) ? $model->getId() : ""); ?>"' class="btn btn-primary">Comment</button>
             <button type="button" onclick='window.location.href="<?php echo url()."/forum"; ?>"' class="btn btn-primary">Close</button>
